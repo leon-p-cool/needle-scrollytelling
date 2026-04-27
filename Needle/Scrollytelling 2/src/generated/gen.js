a65d9b02e82af51e92b6a9cd1552509df4ce0fe2
Praktikant 01 <praktikant.01@int.lefx.de>
2026-04-27 17:34:11 +0200

    initial commit

---
+++ Needle/Scrollytelling 2/src/generated/gen.js
@@ -0,0 +1,27 @@
+﻿/*
+* Developer Information:
+* This file was generated using Unity 6000.0.68f1.
+* Do not modify this file manually.
+
+* Instead of using generated code you can also load the 3D scene like this: <needle-engine src="/assets/your_glTF_name.glb"></needle-engine>
+* (When you're working with Unity the glTF file name will always match your scene or prefab's name)
+*/
+
+globalThis["needle:dependencies:ready"] = import("./register_types.ts")
+
+/** @type {string[]} */
+export const needle_exported_files = new Array();
+globalThis["needle:codegen_files"] = needle_exported_files;
+import "../../assets/Scrollytelling 2.glb?url";
+needle_exported_files.push("./assets/Scrollytelling 2.glb");
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const needleEngine = document.querySelector("needle-engine");
+	if(needleEngine && needleEngine.getAttribute("src") === null)
+	{
+		needleEngine.setAttribute("hash", "1777303955376");
+		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
+	}
+});
+
+console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 5\.0\.4");
